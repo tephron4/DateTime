b2a150adf42d1337071934955e08736cd3a5e8da
tephron4 <tobiasephron@gmail.com>
2020-05-05 20:01:41 -0400

    Create code to display a window with the current date in the middle

--- Date/dateJ1.java
+++ Date/dateJ1.java
@@ -5,17 +5,30 @@
  * Tobias Ephron
  */
 
+import java.util.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import javax.swing.*;
+import java.awt.Font;
 
 public class dateJ1{
 	Scanner s = new Scanner(System.in);
 
 	public static void main(String[] args){
+		JFrame window = new JFrame("Date V1");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(400,400);
+		
 		LocalDate today = LocalDate.now();
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 		String todayString = today.format(dtf);
 		System.out.println(todayString);
+
+		JLabel date = new JLabel(todayString,JLabel.CENTER);
+		date.setFont(new Font("TimesRoman",Font.PLAIN, 32));
+
+		window.add(date);
+
+		window.setVisible(true);
 	}
 }
